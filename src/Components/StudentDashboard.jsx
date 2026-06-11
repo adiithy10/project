9a8056a0744007ddb0d9ff959d2689b9039d2e58
adithy10 <adithysudarsanan@gmail.com>
2026-06-11 13:35:00 +0530

    Initial React project setup by Team Lead

---
+++ src/Components/StudentDashboard.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button';
+
+const StudentDashboard = () => {
+  return (
+    <div>
+        <h1>Dashboard</h1>
+           <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Total Complaints</TableCell>
+              <TableCell>Resolved Complaints</TableCell>
+              <TableCell>Pending Complaints</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>26</TableCell>
+              <TableCell>10</TableCell>
+              <TableCell>16</TableCell>
+            </TableRow>
+                       
+          </TableBody>
+        </Table>
+      </TableContainer>
+      
+     
+    </Paper>
+    <br /> <br /> <br /> <br /> 
+    <Button variant='contained' color='secondary'>Add Complaint</Button>&nbsp;&nbsp;&nbsp;
+    <Button variant='contained' color='error'>Sign Out</Button>
+    </div>
+  )
+}
+
+export default StudentDashboard
