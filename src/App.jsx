0ef1d8fa4a116a70ea562e44921c658f8cf082b7
Adya Arun <168610405+Adyaarun@users.noreply.github.com>
2026-06-11 16:31:24 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import NavBar from './components/NavBar'
 import { Route, Routes } from 'react-router-dom'
-import StudentDashboard from './Components/StudentDashboard'
+import AddComplaint from './components/AddComplaint'
+import ComplainList from './components/ComplainList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<StudentDashboard/>}/>
+        <Route path = "/" element={<h1>Welcome to the Campus Complaint Management System</h1>} />
+        <Route path = "/addcomplaint" element={<AddComplaint />} />
+        <Route path = "/complaintlist" element={<ComplainList />} />     
       </Routes>
     </>
   )
