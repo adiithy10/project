942656016b2ba0a5bcdbef921135167d07f100c1
adithy10 <adithysudarsanan@gmail.com>
2026-06-12 02:19:38 +0530

    Update App.jsx

---
+++ src/Components/Login.jsx
@@ -0,0 +1,46 @@
+import { Button, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import NavBar from './NavBar'
+
+const Login = () => {
+  let navigate = useNavigate()
+
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+  let valueUpdate = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  let submitInfo = () => {
+    console.log('Login form data:', form)
+    if(form.email==='admin@gmail.com'){
+      navigate('/admin')}
+      else{
+        navigate('/StudentDashboard')
+      }
+      }
+      
+    
+   
+  return (
+    <div>
+      <NavBar/>
+      <br /><br /><br /><br />
+      <h3>Login</h3>
+      <br />
+
+      <TextField label="Email" variant='outlined'  value={form.email} onChange={valueUpdate} /><br /><br />
+      <TextField label="Password" type="password" variant='outlined'  value={form.password} onChange={valueUpdate} /><br /><br />
+
+      <Button variant="contained" onClick={submitInfo}>Login</Button>
+      <br /><br />
+      <Button variant="text" onClick={() => navigate('/register')}>Don't have an account? Register</Button>
+    </div>
+  )
+}
+
+export default Login
