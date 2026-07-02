0537826dfdc0b5c35155f473f2998f612691ccb4
Adithy Sudarsanan <adithysudarsanan@gmail.com>
2026-07-03 00:12:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import NavBar from './Components/NavBar'
-import { Route, Routes } from 'react-router-dom'
-import Login from './components/Login'
-import Register from './components/Register'
-import AddComplaint from './Components/AddComplaint'
-import ComplainList from './Components/ComplainList'
-import StudentDashboard from './Components/StudentDashboard'
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import StudentDashboard from "./components/StudentDashboard";
+import AdminDashboard from "./components/AdminDashboard";
+import AddComplaint from "./components/AddComplaint";
+import ComplainList from "./components/ComplainList";
+import AdminComplaintList from "./components/AdminComplaintList";
+import StatusUpdateScreen from "./components/StatusUpdateScreen";
+import MyComplaints from "./components/MyComplaints";
+import ComplaintDetails from "./components/ComplaintDetails";
+import Home from './Components/Home';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path='/'element={<Login/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path = "/" element={<h1>Welcome to the Campus Complaint Management System</h1>} />
-        <Route path = "/addcomplaint" element={<AddComplaint />} />
-        <Route path = "/complaintlist" element={<ComplainList />} />
-        <Route path="/StudentDashboard" element={<StudentDashboard />} />     
-
-      </Routes>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
+      <Route path="/StudentDashboard" element={<StudentDashboard />} />
+      <Route path="/AdminDashboard" element={<AdminDashboard />} />
+      <Route path="/AddComplaint" element={<AddComplaint />} />
+      <Route path="/ComplainList" element={<ComplainList />} />
+      <Route path="/AdminComplaintList" element={<AdminComplaintList />} />
+      <Route path="/StatusUpdateScreen/:id" element={<StatusUpdateScreen />} />
+      <Route path="/MyComplaints" element={<MyComplaints />} />
+      <Route path="/ComplaintDetails/:id" element={<ComplaintDetails />} />
+      
+    </Routes>
+  );
 }
 
-export default App
+export default App;
