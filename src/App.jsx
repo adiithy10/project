80cfc8db1aeb4abe0254294f2c4380c5c58f94e4
Adithy Sudarsanan <adithysudarsanan@gmail.com>
2026-07-03 00:46:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import StudentDashboard from "./components/StudentDashboard";
+import AdminDashboard from "./components/AdminDashboard";
+import AddComplaint from "./components/AddComplaint";
+import ComplainList from "./components/ComplainList";
+import AdminComplaintList from "./components/AdminComplaintList";
+import StatusUpdateScreen from "./components/StatusUpdateScreen";
+import MyComplaints from "./components/MyComplaints";
+import ComplaintDetails from "./components/ComplaintDetails";
+import Home from './Components/Home';
 
-import Login from './Components/Login'
-import Register from './Components/Register'
-import AddComplaint from './Components/AddComplaint'
-import ComplainList from './Components/ComplainList'
-import StudentDashboard from './Components/StudentDashboard'
-import AdminDashboard from './Components/AdminDashboard'
-
-import MyComplaints from "./pages/MyComplaints";
-import ComplaintDetails from "./pages/ComplaintDetails";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<MyComplaints />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-
-      <Route
-        path="/home"
-        element={<h1>Welcome to the Campus Complaint Management System</h1>}
-      />
-
-      <Route path="/addcomplaint" element={<AddComplaint />} />
-      <Route path="/complaintlist" element={<ComplainList />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
       <Route path="/StudentDashboard" element={<StudentDashboard />} />
       <Route path="/AdminDashboard" element={<AdminDashboard />} />
-
-      <Route path="/mycomplaints" element={<MyComplaints />} />
-      <Route path="/complaintdetails/:id" element={<ComplaintDetails />} />
+      <Route path="/AddComplaint" element={<AddComplaint />} />
+      <Route path="/ComplainList" element={<ComplainList />} />
+      <Route path="/AdminComplaintList" element={<AdminComplaintList />} />
+      <Route path="/StatusUpdateScreen/:id" element={<StatusUpdateScreen />} />
+      <Route path="/MyComplaints" element={<MyComplaints />} />
+      <Route path="/ComplaintDetails/:id" element={<ComplaintDetails />} />
+      
     </Routes>
   );
 }
